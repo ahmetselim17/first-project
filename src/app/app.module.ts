310b249dd738cc2b76ef30367e96514afe5a4dd4
Ahmet Selim Taşcı <ahmetselimtasci@gmail.com>
2020-12-05 14:50:28 +0300

    bölüm 9a geçildi bootstrapt toastr spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+import { NgxSpinnerModule } from "ngx-spinner";
+import { IgxButtonModule } from 'igniteui-angular';
+
+
+//I keep the new line
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 3500,
+      progressBar: true,
+      easing: "ease-in",
+      progressAnimation: "increasing",
+      preventDuplicates: true,
+      positionClass: "toast-top-right"
+    }),
+    NgxSpinnerModule,
+    IgxButtonModule
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
